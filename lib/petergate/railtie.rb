56415d70c5a52c89a4e54978577a63c4b27b7d4f
Isaac Sloan <isaac@isaacsloan.com>
2015-01-15 01:06:53 -0700

    set scaffold_generator as default

--- lib/petergate/railtie.rb
+++ lib/petergate/railtie.rb
@@ -1,6 +1,7 @@
 module Petergate
   class Railtie < Rails::Railtie
     config.app_generators do |g|
+      g.scaffold_controller :scaffold_controller
       g.templates.unshift File::expand_path("../templates", File.dirname(__FILE__))
     end 
   end
