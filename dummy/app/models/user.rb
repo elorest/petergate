ff49da3b39a9fb03eb9b2ec33476f994b4c97053
Isaac Sloan <isaac@isaacsloan.com>
2016-04-14 20:29:58 -0600

    better docs for user model

--- dummy/app/models/user.rb
+++ dummy/app/models/user.rb
@@ -1,9 +1,11 @@
 class User < ActiveRecord::Base
-  ################################################################################ 
-  ## PeterGate Roles
-  ## The :user role is added by default and shouldn't be included in this list.
-  petergate(roles: [:root_admin, :company_admin], multiple: true)
-  ################################################################################
+  ############################################################################################
+  ## PeterGate Roles                                                                        ##
+  ## The :user role is added by default and shouldn't be included in this list.             ##
+  ## The :root_admin can access any page regardless of access settings. Use with caution!   ##
+  ## The multiple option can be set to true if you need users to have multiple roles.       ##
+  petergate(roles: [:root_admin, :company_admin], multiple: true)                           ##
+  ############################################################################################ 
  
 
   # Include default devise modules. Others available are:
